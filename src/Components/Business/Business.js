b9532fbb07fe942914bd60d82f5bb00662ccf434
Christsadi <christsadi@gmail.com>
2023-08-11 16:34:49 +0200

    Created business component

---
+++ src/Components/Business/Business.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const businessState = {
+    image: ,
+    name: `Nela's Menu`,
+    address: '1 Gelder close',
+    city: 'Cape Town',
+    state: 'Western Cape',
+    zipcode: '7945',
+    category: 'Homemade',
+    rating: 4.9,
+    reviewCount: 160,
+}
+
+const Business () => {
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Business;
